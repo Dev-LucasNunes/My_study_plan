f25d8a6a6abc90e6ddaf383fedebecd8f1e76d8b
Dev-LucasNunes <dev.lucasnunes@gmail.com>
2024-03-20 19:00:57 -0300

    feat: finish job_sequencing_problem

--- Algoritmos_fase_2/job_sequencing_problem.rb
+++ Algoritmos_fase_2/job_sequencing_problem.rb
@@ -1,14 +1,55 @@
 def job_sequencing_problem(arr, t)
-    arr.sort_by { |subarray| subarray[2]}.reverse
+    sorted_array = arr.sort_by { |subarray| subarray[2]}.reverse
     time_slots = []
     job = []
     i = 0
-    while 1 < t
+    while i < t
         job[i] = "-1"
         time_slots[i] = false
+        i += 1
     end
-    i += 1
+
+    i = 0
+    while i < sorted_array.length
+        j = [t - 1, sorted_array[i][1] - 1].max
+        while j >= 0
+            if time_slots[j] == false
+                time_slots[j] = true
+                job[j] = sorted_array[i][0]
+                break
+            end
+            j -= 1
+            if j < 0
+                break
+            end
+        end
+        i += 1
+    end
+    job
 end
+
+# def job_sequencing_problem(arr, t)
+#   # Ordena o array de trabalhos por lucro em ordem decrescente
+#   sorted_array = arr.sort_by { |subarray| subarray[2] }.reverse
+#   time_slots = Array.new(t, false)
+#   job = Array.new(t, "-1")
+#
+#   sorted_array.each do |job_info|
+#     # Encontra um slot livre para o trabalho atual
+#     j = [t - 1, job_info[1] - 1].max
+#     while j >= 0
+#       if time_slots[j] == false
+#         time_slots[j] = true
+#         job[j] = job_info[0]
+#         break
+#       end
+#       j -= 1
+#     end
+#   end
+#
+#   # Retorna a sequência de trabalhos agendados
+#   job
+# end
 
 arr = [['a', 2, 100],
        ['b', 1, 19],
@@ -16,4 +57,4 @@ arr = [['a', 2, 100],
        ['d', 1, 25],
        ['e', 3, 15]]
 
-puts job_sequencing_problem(arr, 3).inspect
+puts job_sequencing_problem(arr, 3)
--- Algoritmos_fase_2/jump_search.rb
+++ Algoritmos_fase_2/jump_search.rb
@@ -1,25 +1,26 @@
-require 'bundler/setup'
-require 'ruby_algorithm'
+# require 'bundler/setup'
+# require 'ruby_algorithm'
 
-def jump_search(array, k)
-  m = Math.sqrt(array.length).to_i
-  index = 0
+# def jump_search(array, k)
+#   m = Math.sqrt(array.length).to_i
+#   index = 0
 
-  while index < array.length
-    if array[index] > k
-      search_array = array[index - m..index]
-      target = linear_search(search_array, k)
-      if target
-        position = index - m + target[0]
-        return position
-      end
-    end
-    index += m
-  end
-  nil
-end
+#   while index < array.length
+#     if array[index] > k
+#       search_array = array[index - m..index]
+#       target = linear_search(search_array, k)
+#       if target
+#         position = index - m + target[0]
+#         return position
+#       end
+#     end
+#     index += m
+#   end
+#   nil
+# end
 
 
 
-# array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
-# puts jump_search(array, 55)
+# # array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+# # puts jump_search(array, 55)
+
